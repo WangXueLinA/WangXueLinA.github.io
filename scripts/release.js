c5025c6cb8e6dc037e88316dfe91133240f97cad
wxl <xuelinw1125@gmail.com>
2024-06-23 23:54:53 +0800

    feat: 上传

--- scripts/release.js
+++ scripts/release.js
@@ -1,37 +1,37 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable @typescript-eslint/await-thenable */
-const glob = require('glob')
-const inquirer = require('inquirer')
-const cp = require('child_process')
-const { resolve } = require('./utils')
+const glob = require('glob');
+const inquirer = require('inquirer');
+const cp = require('child_process');
+const { resolve } = require('./utils');
 
-const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm'
-const lerna = process.platform === 'win32' ? 'lerna.cmd' : 'npm'
+const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+const lerna = process.platform === 'win32' ? 'lerna.cmd' : 'npm';
 
 const choices = glob.sync(resolve('./packages/*/package.json')).map((p) => ({
-  name: require(p).name
-}))
+  name: require(p).name,
+}));
 const publish = async ({ name }) => {
   await cp.spawnSync(npm, ['publish'], {
     stdio: 'inherit',
-    cwd: resolve(`./packages/${name.replace('@dtd/', '')}`)
-  })
-}
+    cwd: resolve(`./packages/${name.replace('@wxl/', '')}`),
+  });
+};
 inquirer
   .prompt([
     {
       type: 'checkbox',
       message: '请选择要发布的模块(可多选)',
       name: 'modules',
-      choices
-    }
+      choices,
+    },
   ])
   .then(async ({ modules }) => {
     if (modules.length === choices.length) {
       await cp.spawnSync(lerna, ['run', 'publish'], {
-        stdio: 'inherit'
-      })
+        stdio: 'inherit',
+      });
     } else {
-      choices.forEach(publish)
+      choices.forEach(publish);
     }
-  })
+  });
